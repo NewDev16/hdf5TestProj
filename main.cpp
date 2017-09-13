6f4897ed9017e56f5917322555e8e14569bc86c8
FeU-aKlos <andreas.klos@fernuni-hagen.de>
2017-09-13 15:21:45 +0200

    SAVE 2D Array to csv file

--- main.cpp
+++ main.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
+#include <array>
+#include <tuple>
+#include <ctime>
+//#include <malloc.h>
 
+#include <fstream>
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    std::srand(100);
+    const int ARRAY_SIZE_X = 1000;
+    const int ARRAY_SIZE_Y = 1000;
+    //int16_t* testArray = (int16_t*)malloc(sizeof(int16_t*ARRAY_SIZE));
+    std::array<std::array<int16_t,ARRAY_SIZE_Y>,ARRAY_SIZE_X> testArray;
+
+    for (int y = 0; y < ARRAY_SIZE_Y; ++y) {
+        for (int x = 0; x < ARRAY_SIZE_X; ++x) {
+            testArray[x][y] =(int16_t)rand()%INT16_MAX+0;
+        }
+    }
+
+    std::ofstream testFile("test.csv");
+
+
+    if (testFile.is_open()) {
+        for (int y = 0; y < ARRAY_SIZE_Y; ++y) {
+            for (int x = 0; x < ARRAY_SIZE_X; ++x) {
+                testFile << testArray[x][y]  << ";";
+            }
+            testFile << "\n";
+        }
+
+        testFile.close();
+    }
+    else std::cerr << "Unable to open file!!!" << std::endl;
+
     return 0;
 }
